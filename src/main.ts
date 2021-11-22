afa2005c3c7e1045d9a2a906c3bfa3b2782aaf76
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-11-22 13:21:44 +0100

    chore(update-config) update env

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('APP_PORT');
 
   app.enableVersioning({
     type: VersioningType.URI,
@@ -17,6 +13,6 @@ async function bootstrap() {
 
   app.enableCors();
 
-  await app.listen(PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
