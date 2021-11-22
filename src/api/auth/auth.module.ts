2e81113a30fa8fa8ca22bcba33bdebfd115fad7c
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-11-22 11:16:14 +0100

    chore(update-env): update enviromental variables

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: async (configService: ConfigService) => {
         return {
           secret: configService.get<string>('JWT_SECRET'),
-          signOptions: { expiresIn: '1d' },
+          signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES') },
         };
       },
       inject: [ConfigService],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('APP_PORT');
 
   app.enableVersioning({
     type: VersioningType.URI,
@@ -13,6 +17,6 @@ async function bootstrap() {
 
   app.enableCors();
 
-  await app.listen(6000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
